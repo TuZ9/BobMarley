0aa55c9181bb9fb6fc5cb2d39d84dc0ad33e6d4b
TuZ9 <arthur.vaz95@outlook.com>
2023-11-15 13:20:31 -0300

    Serilog

---
+++ Infra/Ioc/Serilog/SerilogExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog.Events;
+using Serilog.Filters;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BobMarley.Infra.Ioc.Serilog
+{
+    public class SerilogExtension
+    {
+        public static void AddSerilog(IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", $"API Serilog - {Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT")}")
+                .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.StaticFiles"))
+                .Filter.ByExcluding(z => z.MessageTemplate.Text.Contains("Business error"))
+                .WriteTo.Console()
+                .CreateLogger();
+        }
+    }
+}
