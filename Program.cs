e665a06c59263effa1ec4149c18ecbd993e4a27c
TuZ9 <arthur.vaz95@outlook.com>
2023-11-15 13:26:57 -0300

    add Serilog

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BobMarley.Infra.Ioc;
+using BobMarley.Infra.Ioc.Serilog;
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+SerilogExtension.AddSerilog(builder.Configuration);
+
 Console.WriteLine("Hello, World!");
