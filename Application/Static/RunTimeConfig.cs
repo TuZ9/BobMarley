1b553bd4f06c2b0c2a58f43d1917ff01a01a1a1a
TuZ9 <arthur.vaz95@outlook.com>
2023-12-04 16:05:43 -0300

    Add swagger

---
+++ Application/Static/RunTimeConfig.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace BobMarley.Application.Static
+{
+    public static class RunTimeConfig
+    {
+        public static string Auroraconnection = "";
+        public static string Mongoconnection = "";
+
+        public static void SetCOnfigs(ConfigurationManager configuration)
+        {
+            if (Debugger.IsAttached)
+            {
+                Auroraconnection = configuration.GetConnectionString("Auroraconnection");
+                Mongoconnection = configuration.GetConnectionString("Mongoconnection");
+            }
+            else
+            {
+                Auroraconnection = Environment.GetEnvironmentVariable("Auroraconnection");
+                Mongoconnection = Environment.GetEnvironmentVariable("Mongoconnection");
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
 ﻿using BobMarley.Infra.Ioc.Serilog;
+using BobMarley.Infra.Ioc.Swagger;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 SerilogExtension.AddSerilog(builder.Configuration);
+SwaggerConfiguration.AddSwagger(builder.Services);
 
-Console.WriteLine("Hello, World!");
+builder.Services.AddMemoryCache();
+builder.Services.AddHealthChecks();
+builder.Services.AddControllers();
+builder.Services.AddCors(options => options.AddPolicy("All", opt => opt
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials()
+                        .SetIsOriginAllowed(hostname => true)));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors("All");
+app.MapControllers();
+app.Run();
